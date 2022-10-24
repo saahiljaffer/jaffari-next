51f1445806c2aefb7a74e5337dc8da51af541a50
Saahil Jaffer <contact@saahiljaffer.com>
2022-10-23 21:48:46 -0400

    chore(prettify): fix linter error

--- src/components/Agenda/Agenda.tsx
+++ src/components/Agenda/Agenda.tsx
@@ -1,52 +1,52 @@
-import momentHijri from "moment-hijri";
+import momentHijri from 'moment-hijri';
 
-import { AgendaItem } from "./AgendaItem";
+import { AgendaItem } from './AgendaItem';
 
 const events = [
   {
     id: 0,
-    title: "Thursday Night",
+    title: 'Thursday Night',
     start: new Date(2022, 9, 27, 19, 15, 0, 0),
     end: new Date(2022, 9, 27, 22, 30, 0, 0),
     desc: [
-      "7:15pm – Salat, Ziarat E Warith, Sura Yasin, Dua Kumail, Majlis by Syed Asad Jafry and Ziarat.",
+      '7:15pm – Salat, Ziarat E Warith, Sura Yasin, Dua Kumail, Majlis by Syed Asad Jafry and Ziarat.',
     ],
     live: true,
   },
   {
     id: 1,
-    title: "Jumuah",
+    title: 'Jumuah',
     start: new Date(2022, 9, 28, 12, 30, 0, 0),
     end: new Date(2022, 9, 28, 14, 30, 0, 0),
     desc: [
-      "12:30pm – Du’ā-e-Nudba.",
-      "1:02pm – Jumu’ah Salāt led by Maulana Sayyid Muhammad Rizvi,  followed by Friday Ziyārat.",
+      '12:30pm – Du’ā-e-Nudba.',
+      '1:02pm – Jumu’ah Salāt led by Maulana Sayyid Muhammad Rizvi,  followed by Friday Ziyārat.',
     ],
     live: true,
   },
   {
     id: 2,
-    title: "Thursday Night",
+    title: 'Thursday Night',
     start: new Date(2022, 10, 3, 19, 15, 0, 0),
     end: new Date(2022, 10, 3, 22, 30, 0, 0),
     desc: [
-      "7:15pm – Salat, Ziarat E Warith, Sura Yasin, Dua Kumail, Majlis by Syed Asad Jafry and Ziarat.",
+      '7:15pm – Salat, Ziarat E Warith, Sura Yasin, Dua Kumail, Majlis by Syed Asad Jafry and Ziarat.',
     ],
     live: true,
   },
   {
     id: 3,
-    title: "Wiladat of Imam Hassan Askari",
+    title: 'Wiladat of Imam Hassan Askari',
     start: new Date(2022, 10, 4, 19, 15, 0, 0),
     end: new Date(2022, 10, 4, 22, 30, 0, 0),
     desc: [
-      "7:15pm – Salat, Ziarat E Warith, Sura Yasin, Dua Kumail, Majlis by Syed Asad Jafry and Ziarat.",
+      '7:15pm – Salat, Ziarat E Warith, Sura Yasin, Dua Kumail, Majlis by Syed Asad Jafry and Ziarat.',
     ],
     live: true,
   },
 ];
 
-momentHijri.locale("en");
+momentHijri.locale('en');
 
 function Agenda() {
   return (
